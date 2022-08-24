7ce5a2ae4fccc3349f4a432e1f57bfdee2ccb9aa
Miguel Ramirez <mramirez@facturaxion.com>
2022-08-23 19:38:41 -0500

    Register, Login and password reset templates completed

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,16 +1,25 @@
 
 const formularioLogin = (req, res) => {
     res.render('auth/login', {
+        pagina:'Iniciar Sesión'
     })
 }
 
 const formularioRegistro = (req, res) => {
     res.render('auth/registro', {
+        pagina:'Crear Cuenta'
+    })
+}
+
+const formularioOlvidePassword = (req, res) => {
+    res.render('auth/olvide-password', {
+        pagina:'Recupera tu acceso a Bienes Raices'
     })
 }
 
 
 export {
     formularioLogin,
-    formularioRegistro
+    formularioRegistro,
+    formularioOlvidePassword
 }
